ad97e08e3483f50265fcd8a340c2397a056798b7
Yuval Kashtan <yuval.kashtan@model9.io>
2019-04-04 00:33:52 +0300

    add templates with ejs

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const bodyParser = require('body-parser');
 const express = require('express');
+
 const poalim = require('./poalim');
 
 const subscriptionKey = process.env.SUBSCRIPTION_KEY;
@@ -10,6 +11,7 @@ const client_secret = process.env.CLIENT_SECRET;
 
 var app = express();
 app.set('port', process.env.PORT || 5000);
+app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
 
@@ -18,7 +20,9 @@ app.get('/getBranchInfo', async function(req, res) {
     console.log('bank: ' + req.query.bank_id + ', branch: ' + req.query.branch_id);
 
     var access_token = await poalim.auth();
-    res.status(200).send(await poalim.getBranchInfo(access_token, req.query.bank_id, req.query.branch_id));
+    var branch_info = await poalim.getBranchInfo(access_token, req.query.bank_id, req.query.branch_id)
+
+    res.render('branchinfo', { branchName: branch_info.branchName });
 });
 
 app.listen(app.get('port'), function() {
